5d0b701dba05ed93b960fe0a4ef64bd243c363b6
username <useremail@example.com>
2024-02-17 03:42:24 +0300

    Homework3

--- hw-3.js
+++ hw-3.js
@@ -36,41 +36,25 @@ if ( question === password ) {
 var monthNumber = Number(prompt("Введите номер месяца"));
 
 switch (monthNumber) {
+   case '12':
    case '1':
-      console.log('зима');
-      break;
    case '2':
       console.log('зима');
       break;
    case '3':
-      console.log('весна');
-      break;
    case '4':
-      console.log('весна');
-       break;
    case '5':
       console.log('весна');
       break;
    case '6':
-      console.log('лето');
-      break;
    case '7':
-      console.log('лето');
-      break;
    case '8':
       console.log('лето');
       break;
    case '9':
-      console.log('осень');
-      break;
    case '10':
-      console.log('осень');
-      break;
    case '11':
       console.log('осень');
-      break;
-   case '12':
-      console.log('зима');
       break;
    default:
       console.log('Ошибка');
